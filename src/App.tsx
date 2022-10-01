d7453c6e76547131294c59c4e5f9c5343dc92ade
dbali00 <dbali00@bhnetwork.com>
2022-10-01 23:34:52 +0530

    complete challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  const [colors,setColors] = useState<string[]>([]);
+  const [ans, setAns] = useState<string>('');
+  const [result, setResult] = useState<string>('');
+  useEffect(() => {
+    const colorss = [generateColor(), generateColor(),generateColor()];
+    setColors(colorss);
+    setAns(colorss[Math.floor(Math.random() * colors.length)]);
+  },[]);
+  const clickHandler = (data:string) => {
+      if(data === ans){
+        setResult('Right');
+      } else{
+        setResult('Wrong');
+      }
+      const colorss = [generateColor(), generateColor(),generateColor()];
+    setColors(colorss);
+    setAns(colorss[Math.floor(Math.random() * colors.length)]);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="col">
+      <div className="box" style={{backgroundColor: ans}}>
+     </div>
+     <div className="buttons">
+      <button onClick={() => clickHandler(colors[0])}>{colors[0]}</button>
+      <button onClick={() => clickHandler(colors[1])}>{colors[1]}</button>
+      <button onClick={() => clickHandler(colors[2])}>{colors[2]}</button>
+     </div>
+     <div className="result">
+      {result !== ''? <p>{result}</p>:null}
+     </div>
+      </div>
+   
     </div>
   );
+}
+
+function generateColor(){
+  const letters = ['1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+  let color = "";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * letters.length)];
+  }
+  return "#"+color;
 }
 
 export default App;
